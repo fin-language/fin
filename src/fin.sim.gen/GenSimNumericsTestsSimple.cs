35153be1047d101ddec384e9bfda2e3c853462f7
adam fraser-kruck <adamfk@gmail.com>
2023-10-13 22:24:36 -0500

    test gen wip

--- src/fin.sim.gen/GenSimNumericsTestsSimple.cs
+++ src/fin.sim.gen/GenSimNumericsTestsSimple.cs
@@ -9,9 +9,34 @@ public class GenSimNumericsTestsSimple
     {
         var path = TestHelper.GetThisDir() + "../fin.sim.test/IntegerCombinationTest.cs";
 
-        string code = "";
-
         var types = GenSimNumerics.types;
+        string test1 = GenTest1Code(types);
+        string sameSignLiteralTest = GenSameSignLiteralTestCode(types);
+        string unsignedVarSignedLiteralTest = GenUnsignedVarSignedLiteralTestCode(types);
+
+        var fileTemplate = $$"""
+            // NOTE!!! Auto generated
+            namespace fin.sim.test;
+
+            public class IntegerCombinationTest
+            {
+                {{test1.IndentNewLines("    ")}}
+                {{sameSignLiteralTest.IndentNewLines("    ")}}
+                {{unsignedVarSignedLiteralTest.IndentNewLines("    ")}}
+            }
+            """;
+
+        File.WriteAllText(path: path, fileTemplate);
+    }
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <param name="types"></param>
+    /// <returns></returns>
+    private static string GenTest1Code(TypeInfo[] types)
+    {
+        string code = "";
 
         foreach (var type in types)
         {
@@ -31,34 +56,127 @@ public class GenSimNumericsTestsSimple
             }
         }
 
-        var template = $$"""
-            // NOTE!!! Auto generated
-            using FluentAssertions;
-            using Xunit;
-            using fin.sim.lang;
-
-            namespace fin.sim.test;
-
-            public class IntegerCombinationTest
+        var test1 = $$"""
+            [Fact]
+            public void Test1()
             {
-                [Fact]
-                public void Test1()
-                {
-                    math.unsafe_mode();
-                    i8 i8 = 1;
-                    i16 i16 = 1;
-                    i32 i32 = 1;
-                    i64 i64 = 1;
-                    u8 u8 = 1;
-                    u16 u16 = 1;
-                    u32 u32 = 1;
-                    u64 u64 = 1;
+                math.unsafe_mode();
+                i8 i8 = 1;
+                i16 i16 = 1;
+                i32 i32 = 1;
+                i64 i64 = 1;
+                u8 u8 = 1;
+                u16 u16 = 1;
+                u32 u32 = 1;
+                u64 u64 = 1;
 
-                    {{code.IndentNewLines("        ")}}
-                }
+                {{code.IndentNewLines("    ")}}
             }
             """;
+        return test1;
+    }
 
-        File.WriteAllText(path:path, template);
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <param name="types"></param>
+    /// <returns></returns>
+    private static string GenSameSignLiteralTestCode(TypeInfo[] types)
+    {
+        string code = "";
+
+        foreach (var type in types)
+        {
+            foreach (var type2 in types)
+            {
+                if (type.is_signed != type2.is_signed)
+                    continue;
+
+                var resultType = type.GetResultType(type2);
+
+                if (resultType.width > 64)
+                    code += "//";
+
+                code += $"{{ var c = {type.fin_name} + {type2.GetMaxValue() - 1}; c.Should().BeOfType<{resultType.fin_name}>(); c.Should().Be({type2.GetMaxValue()}); }}";
+
+                if (resultType.width > 64)
+                    code += "  // not allowed for now (need 128 bit or extra logic)";
+
+                code += "\n";
+            }
+        }
+
+        var testCode = $$"""
+            [Fact]
+            public void SameSignLiteralTest()
+            {
+                math.unsafe_mode();
+                //i8 i8 = 1;
+                //i16 i16 = 1;
+                //i32 i32 = 1;
+                //i64 i64 = 1;
+                u8 u8 = 1;
+                u16 u16 = 1;
+                u32 u32 = 1;
+                u64 u64 = 1;
+
+                {{code.IndentNewLines("    ")}}
+            }
+            """;
+        return testCode;
+    }
+
+
+    /// <summary>
+    /// TODO
+    /// </summary>
+    /// <param name="types"></param>
+    /// <returns></returns>
+    private static string GenUnsignedVarSignedLiteralTestCode(TypeInfo[] types)
+    {
+        string code = "";
+
+        foreach (var type in types)
+        {
+            if (type.is_signed)
+                continue;
+
+            foreach (var type2 in types)
+            {
+                if (!type2.is_signed)
+                    continue;
+
+                var resultType = type.GetResultType(type2);
+
+                if (resultType.width > 64)
+                    code += "//";
+
+                code += $"{{ var c = {type.fin_name} + {type2.GetMinValue()}; c.Should().BeOfType<{resultType.fin_name}>(); c.Should().Be({type2.GetMaxValue() + 1}); }}";
+
+                if (resultType.width > 64)
+                    code += "  // not allowed for now (need 128 bit or extra logic)";
+
+                code += "\n";
+            }
+        }
+
+        var testCode = $$"""
+            [Fact]
+            public void DiffSignLiteralTest()
+            {
+                math.unsafe_mode();
+                i8 i8 = 1;
+                i16 i16 = 1;
+                i32 i32 = 1;
+                i64 i64 = 1;
+                u8 u8 = 1;
+                u16 u16 = 1;
+                u32 u32 = 1;
+                u64 u64 = 1;
+
+                {{code.IndentNewLines("    ")}}
+            }
+            """;
+        return testCode;
     }
 }
--- src/fin.sim.test/IntegerCombinationTest.cs
+++ src/fin.sim.test/IntegerCombinationTest.cs
@@ -1,8 +1,4 @@
 // NOTE!!! Auto generated
-using FluentAssertions;
-using Xunit;
-using fin.sim.lang;
-
 namespace fin.sim.test;
 
 public class IntegerCombinationTest
@@ -19,7 +15,7 @@ public class IntegerCombinationTest
         u16 u16 = 1;
         u32 u32 = 1;
         u64 u64 = 1;
-
+    
         { var c = i8 + i8; c.Should().BeOfType<i8>(); }
         { var c = i8 + i16; c.Should().BeOfType<i16>(); }
         { var c = i8 + i32; c.Should().BeOfType<i32>(); }
@@ -84,6 +80,84 @@ public class IntegerCombinationTest
         { var c = u64 + u16; c.Should().BeOfType<u64>(); }
         { var c = u64 + u32; c.Should().BeOfType<u64>(); }
         { var c = u64 + u64; c.Should().BeOfType<u64>(); }
+        
+    }
+    [Fact]
+    public void SameSignLiteralTest()
+    {
+        math.unsafe_mode();
+        i8 i8 = 1;
+        i16 i16 = 1;
+        i32 i32 = 1;
+        i64 i64 = 1;
+        u8 u8 = 1;
+        u16 u16 = 1;
+        u32 u32 = 1;
+        u64 u64 = 1;
+    
+        { var c = i8 + 126; c.Should().BeOfType<i8>(); c.Should().Be(127); }
+        { var c = i8 + 32766; c.Should().BeOfType<i16>(); c.Should().Be(32767); }
+        { var c = i8 + 2147483646; c.Should().BeOfType<i32>(); c.Should().Be(2147483647); }
+        { var c = i8 + 9223372036854775806; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775807); }
+        { var c = i16 + 126; c.Should().BeOfType<i16>(); c.Should().Be(127); }
+        { var c = i16 + 32766; c.Should().BeOfType<i16>(); c.Should().Be(32767); }
+        { var c = i16 + 2147483646; c.Should().BeOfType<i32>(); c.Should().Be(2147483647); }
+        { var c = i16 + 9223372036854775806; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775807); }
+        { var c = i32 + 126; c.Should().BeOfType<i32>(); c.Should().Be(127); }
+        { var c = i32 + 32766; c.Should().BeOfType<i32>(); c.Should().Be(32767); }
+        { var c = i32 + 2147483646; c.Should().BeOfType<i32>(); c.Should().Be(2147483647); }
+        { var c = i32 + 9223372036854775806; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775807); }
+        { var c = i64 + 126; c.Should().BeOfType<i64>(); c.Should().Be(127); }
+        { var c = i64 + 32766; c.Should().BeOfType<i64>(); c.Should().Be(32767); }
+        { var c = i64 + 2147483646; c.Should().BeOfType<i64>(); c.Should().Be(2147483647); }
+        { var c = i64 + 9223372036854775806; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775807); }
+        { var c = u8 + 254; c.Should().BeOfType<u8>(); c.Should().Be(255); }
+        { var c = u8 + 65534; c.Should().BeOfType<u16>(); c.Should().Be(65535); }
+        { var c = u8 + 4294967294; c.Should().BeOfType<u32>(); c.Should().Be(4294967295); }
+        { var c = u8 + 18446744073709551614; c.Should().BeOfType<u64>(); c.Should().Be(18446744073709551615); }
+        { var c = u16 + 254; c.Should().BeOfType<u16>(); c.Should().Be(255); }
+        { var c = u16 + 65534; c.Should().BeOfType<u16>(); c.Should().Be(65535); }
+        { var c = u16 + 4294967294; c.Should().BeOfType<u32>(); c.Should().Be(4294967295); }
+        { var c = u16 + 18446744073709551614; c.Should().BeOfType<u64>(); c.Should().Be(18446744073709551615); }
+        { var c = u32 + 254; c.Should().BeOfType<u32>(); c.Should().Be(255); }
+        { var c = u32 + 65534; c.Should().BeOfType<u32>(); c.Should().Be(65535); }
+        { var c = u32 + 4294967294; c.Should().BeOfType<u32>(); c.Should().Be(4294967295); }
+        { var c = u32 + 18446744073709551614; c.Should().BeOfType<u64>(); c.Should().Be(18446744073709551615); }
+        { var c = u64 + 254; c.Should().BeOfType<u64>(); c.Should().Be(255); }
+        { var c = u64 + 65534; c.Should().BeOfType<u64>(); c.Should().Be(65535); }
+        { var c = u64 + 4294967294; c.Should().BeOfType<u64>(); c.Should().Be(4294967295); }
+        { var c = u64 + 18446744073709551614; c.Should().BeOfType<u64>(); c.Should().Be(18446744073709551615); }
+        
+    }
+    [Fact]
+    public void DiffSignLiteralTest()
+    {
+        math.unsafe_mode();
+        i8 i8 = 1;
+        i16 i16 = 1;
+        i32 i32 = 1;
+        i64 i64 = 1;
+        u8 u8 = 1;
+        u16 u16 = 1;
+        u32 u32 = 1;
+        u64 u64 = 1;
+    
+        { var c = u8 + -128; c.Should().BeOfType<i16>(); c.Should().Be(128); }
+        { var c = u8 + -32768; c.Should().BeOfType<i16>(); c.Should().Be(32768); }
+        { var c = u8 + -2147483648; c.Should().BeOfType<i32>(); c.Should().Be(2147483648); }
+        { var c = u8 + -9223372036854775808; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775808); }
+        { var c = u16 + -128; c.Should().BeOfType<i32>(); c.Should().Be(128); }
+        { var c = u16 + -32768; c.Should().BeOfType<i32>(); c.Should().Be(32768); }
+        { var c = u16 + -2147483648; c.Should().BeOfType<i32>(); c.Should().Be(2147483648); }
+        { var c = u16 + -9223372036854775808; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775808); }
+        { var c = u32 + -128; c.Should().BeOfType<i64>(); c.Should().Be(128); }
+        { var c = u32 + -32768; c.Should().BeOfType<i64>(); c.Should().Be(32768); }
+        { var c = u32 + -2147483648; c.Should().BeOfType<i64>(); c.Should().Be(2147483648); }
+        { var c = u32 + -9223372036854775808; c.Should().BeOfType<i64>(); c.Should().Be(9223372036854775808); }
+        //{ var c = u64 + -128; c.Should().BeOfType<i128>(); c.Should().Be(128); }  // not allowed for now (need 128 bit or extra logic)
+        //{ var c = u64 + -32768; c.Should().BeOfType<i128>(); c.Should().Be(32768); }  // not allowed for now (need 128 bit or extra logic)
+        //{ var c = u64 + -2147483648; c.Should().BeOfType<i128>(); c.Should().Be(2147483648); }  // not allowed for now (need 128 bit or extra logic)
+        //{ var c = u64 + -9223372036854775808; c.Should().BeOfType<i128>(); c.Should().Be(9223372036854775808); }  // not allowed for now (need 128 bit or extra logic)
         
     }
 }
