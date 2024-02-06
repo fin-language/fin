328edfd78341726955541dff5967cad616ee3504
adam fraser-kruck <adamfk@gmail.com>
2024-02-06 14:43:32 -0600

    example - setup Transpiler console application

--- src/test/ExSln2/TranspileRunner/Program.cs
+++ src/test/ExSln2/TranspileRunner/Program.cs
@@ -1,4 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using finlang;
+using finlang.Transpiler;
+
 Console.WriteLine("Hello, World!");
 
-//FinTransipler.TranspileProject("./LedBlinker.csproj");
+string thisDir = PathHelpers.GetThisDir();
+
+string slnDir = PathHelpers.GetThisDir() + "/../";
+string slnName = "ExSln2.sln";
+string outDir = slnDir + "/c99/gen";
+string projectName = "LedBlinker";
+
+Transpiler transpiler = new(destinationDirPath: outDir, solutionPath: slnDir + slnName, projectName: projectName);
+
+transpiler.GenerateAndWrite();
+var fileNames = transpiler.GetListOfAllGeneratedFiles();
+
+Console.WriteLine("Generated files:");
+foreach (var fileName in fileNames)
+{
+    Console.WriteLine(fileName);
+}
+Console.WriteLine("Done!");
