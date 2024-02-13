d87418d5dc6e643d4549b170f542d26a58d5f95a
adam fraser-kruck <adamfk@gmail.com>
2024-02-13 08:44:35 -0600

    add var keyword example

---
+++ src/test/ExSln2/c99/gen/hal_VarKeywordEx.c
@@ -0,0 +1,11 @@
+// finlang generated file for c# hal.VarKeywordEx type
+
+#include "hal_VarKeywordEx.h"
+
+
+int16_t hal_VarKeywordEx_add(uint8_t a, int8_t b)
+{
+    //fin: var result = a + b;
+    int16_t result = a + b; // should end up as i16
+    return result;
+}
---
+++ src/test/ExSln2/c99/gen/hal_VarKeywordEx.h
@@ -0,0 +1,10 @@
+// finlang generated file for c# hal.VarKeywordEx type
+#pragma once
+
+#include <stdint.h>
+#include <stdint.h>
+#include <stdint.h>
+
+
+// Class has no fields. No struct generated.
+int16_t hal_VarKeywordEx_add(uint8_t a, int8_t b);
