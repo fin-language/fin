ba72ec53e7f75255b51529969e7432d46d72aade
adam fraser-kruck <adamfk@gmail.com>
2024-02-23 14:32:42 -0600

    example - interface work GpioDigInOut

---
+++ src/test/ExSln2/c99/gen/hal_GpioDigInOut.c
@@ -0,0 +1,34 @@
+// finlang generated file for c# hal.GpioDigInOut type
+
+#include "hal_GpioDigInOut.h"
+#include <string.h>
+
+
+
+void hal_GpioDigInOut_ctor(hal_GpioDigInOut * self, hal_Gpio * gpio)
+{
+    memset(self, 0, sizeof(*self));
+    self->_gpio = gpio;
+}
+
+bool hal_GpioDigInOut_read_state(hal_GpioDigInOut * self)
+{
+    return hal_Gpio_read(self->_gpio) == hal_GpioPinState_High;
+}
+
+void hal_GpioDigInOut_set_state(hal_GpioDigInOut * self, bool state)
+{
+    hal_Gpio_write(self->_gpio, state ? hal_GpioPinState_High : hal_GpioPinState_Low);
+}
+
+void hal_GpioDigInOut_toggle(hal_GpioDigInOut * self)
+{
+    hal_GpioPinState next_state = hal_Gpio_read(self->_gpio) == hal_GpioPinState_High ? hal_GpioPinState_Low : hal_GpioPinState_High;
+    hal_Gpio_write(self->_gpio, next_state);
+}
+
+[finlang_simonlyAttribute_ctor]
+void PRIVATE_hal_GpioDigInOut__ThrowIfDestructed(hal_GpioDigInOut * self)
+{
+    throw new System_NotImplementedException *();
+}
---
+++ src/test/ExSln2/c99/gen/hal_GpioDigInOut.h
@@ -0,0 +1,25 @@
+// finlang generated file for c# hal.GpioDigInOut type
+#pragma once
+
+#include "hal_Gpio.h"
+#include <stdbool.h>
+
+
+
+typedef struct hal_GpioDigInOut hal_GpioDigInOut;
+struct hal_GpioDigInOut
+{
+    hal_Gpio * _gpio;
+};
+
+
+void hal_GpioDigInOut_ctor(hal_GpioDigInOut * self, hal_Gpio * gpio);
+
+bool hal_GpioDigInOut_read_state(hal_GpioDigInOut * self);
+
+void hal_GpioDigInOut_set_state(hal_GpioDigInOut * self, bool state);
+
+void hal_GpioDigInOut_toggle(hal_GpioDigInOut * self);
+
+[finlang_simonlyAttribute_ctor]
+void PRIVATE_hal_GpioDigInOut__ThrowIfDestructed(hal_GpioDigInOut * self);
