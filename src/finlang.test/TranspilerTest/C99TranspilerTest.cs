356a42c699fccd96b3cff13829a07ad7de977436
Adam Fraser-Kruck <adamfk@gmail.com>
2024-08-16 17:22:48 -0500

    test fix

--- src/finlang.test/TranspilerTest/C99TranspilerTest.cs
+++ src/finlang.test/TranspilerTest/C99TranspilerTest.cs
@@ -4,31 +4,32 @@ namespace finlang.TranspilerTest;
 
 public class C99TranspilerTest
 {
-    /// <summary>
-    /// https://github.com/fin-language/fin/issues/75
-    /// </summary>
-    [Fact]
-    public void NoCFileForPlainOldDataTypes_75()
-    {
-        const string code = """
-            using finlang;
+    // Disabled in favor of default constructors https://github.com/fin-language/fin/issues/32
+    ///// <summary>
+    ///// https://github.com/fin-language/fin/issues/75
+    ///// </summary>
+    //[Fact]
+    //public void NoCFileForPlainOldDataTypes_75()
+    //{
+    //    const string code = """
+    //        using finlang;
 
-            public class MyClass: FinObj
-            {
-                public u8 id;
-                public u16 count;
-            }
-                
-            // to satisfy the compiler
-            class DummyMain
-            {
-                static void Main(string[] args){}
-            }
-            """;
+    //        public class MyClass: FinObj
+    //        {
+    //            public u8 id;
+    //            public u16 count;
+    //        }
 
-        var files = TranspilerTestHelper.TranspileFinToCFiles(code);
+    //        // to satisfy the compiler
+    //        class DummyMain
+    //        {
+    //            static void Main(string[] args){}
+    //        }
+    //        """;
 
-        files.HasFileName("MyClass.h").Should().BeTrue();
-        files.HasFileName("MyClass.c").Should().BeFalse();
-    }
+    //    var files = TranspilerTestHelper.TranspileFinToCFiles(code);
+
+    //    files.HasFileName("MyClass.h").Should().BeTrue();
+    //    files.HasFileName("MyClass.c").Should().BeFalse();
+    //}
 }
